ba8e5e58803e2c87b282bc8885bff9c451d901fd
Nafeu Nasir <nafeu.nasir@gmail.com>
2020-09-27 15:31:25 -0400

    Add title icon and styling changes

--- client/src/components/title/index.js
+++ client/src/components/title/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
 
 const Title = ({ title }) => {
-  return <div className="title">{title}</div>;
+  return (
+    <div className="title">
+      <FontAwesomeIcon icon={faMarkdown} /> {title}
+    </div>
+  );
 };
 
 export default Title;
